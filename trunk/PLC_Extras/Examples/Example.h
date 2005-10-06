a67cfb47e76d1b2c1bcf43b1e726b5dd083918cd
bdesgraupes <bdesgraupes@2141b295-cd36-0410-9b69-904a98297b35>
2005-10-06 09:20:20 +0000

    Initial revision

---
+++ trunk/PLC_Extras/Examples/Example.h
@@ -0,0 +1,29 @@
+// ===========================================================================
+// Example.h
+//                       Created: 2005-10-05 12:26:37
+//             Last modification: 2005-10-05 12:32:29
+// Author: Bernard Desgraupes
+// e-mail: <bdesgraupes@easyconnect.fr>
+// www: <http://webperso.easyconnect.fr/bdesgraupes/>
+// © Copyright: Bernard Desgraupes 2005
+// All rights reserved.
+// $Date$
+// $Revision$
+// ===========================================================================
+
+// Bundle identifier string
+#define kBundleIdentifier "net.sourceforge.plistcompiler"
+
+// Version number
+#define kMajorVersion "0"
+#define kMinorVersion "1"
+#define kSubMinorVersion "1"
+
+// Development stages
+#define kAlphaStage 0x40
+#define kBetaStage 0x60
+#define kFinalStage 0x80
+
+#define kCurrentStage kBetaStage
+
+
