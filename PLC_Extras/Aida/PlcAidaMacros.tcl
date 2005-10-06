27f01fe11629452457f37a8fdd9084b6c4979270
bdesgraupes <bdesgraupes@2141b295-cd36-0410-9b69-904a98297b35>
2005-10-06 09:20:20 +0000

    Initial revision

---
+++ PLC_Extras/Aida/PlcAidaMacros.tcl
@@ -0,0 +1,21 @@
+# ===========================================================================
+# File: "PlcAidaMacros.tcl"
+#                        Created: 2005-10-05 15:39:47
+#              Last modification: 2005-10-06 00:11:07
+# Author: Bernard Desgraupes
+# e-mail: <bdesgraupes@easyconnect.fr>
+# www: <http://webperso.easyconnect.fr/bdesgraupes/>
+# (c) Copyright: Bernard Desgraupes 2005
+# All rights reserved.
+# $Date$
+# $Revision$
+# ===========================================================================
+
+namespace eval plc {}
+
+proc plc::getVersion {} {
+	global plc
+	set aidafile [win::Current]
+	catch {source [file normalize [file join [file dir $aidafile] ../../plcompile ]]}
+	return $plc(version)
+}
