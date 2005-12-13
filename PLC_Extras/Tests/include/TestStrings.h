1e9e74f61930496010766609accf1dd3c0c84520
bdesgraupes <bdesgraupes@2141b295-cd36-0410-9b69-904a98297b35>
2005-12-13 08:36:58 +0000

    Removed expandable kw

--- PLC_Extras/Tests/include/TestStrings.h
+++ PLC_Extras/Tests/include/TestStrings.h
@@ -1,22 +1,19 @@
 // ===========================================================================
 // TestStrings.h
 //                       Created: 2005-11-29 17:51:27
-//             Last modification: 2005-11-29 18:04:01
+//             Last modification: 2005-12-13 09:18:02
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@easyconnect.fr>
 // www: <http://webperso.easyconnect.fr/bdesgraupes/>
 // © Copyright: Bernard Desgraupes 2005
 // All rights reserved.
-// $Date$
-// $Revision$
 // ===========================================================================
 // This file is included by the Test_include<>.plc via an include macro 
 // using angle brackets. It should be located on the paths contained in 
 // the HEADER_SEARCH_PATHS variable. In the  test project, it has been set 
 // to:
 // "${SRCROOT}/../../PLC_Extras/Tests/include/"
-// 
-// setenv SRCROOT /Volumes/im2/Devel/Current/PListCompiler/PListCompiler_Devel/PLC_Sources/TestPlc
+
 
 #define STRING_ONE "A first line of text"
 #define STRING_TWO "A second line"
