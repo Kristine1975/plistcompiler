0a95fd077837b1b8d962415b6fa89d37d93cdd4c
bdesgraupes <bdesgraupes@2141b295-cd36-0410-9b69-904a98297b35>
2007-09-11 07:03:18 +0000

    Misc test modifs before 0.6 release

--- PLC_Extras/Tests/Test_ShellPhase.sh
+++ PLC_Extras/Tests/Test_ShellPhase.sh
@@ -1,14 +1,19 @@
 cd $SRCROOT
 cd ..
 testname="if_nested"
-infoplist=${BUILT_PRODUCTS_DIR}/${CONTENTS_FOLDER_PATH}/Info.plist
+infoplist=${TARGET_BUILD_DIR}/${CONTENTS_FOLDER_PATH}/Info.plist
 
 if [ -f ${infoplist} ] ; then
 	rm -f ${infoplist}
 fi
 
-./plcompile ../PLC_Extras/Tests/Test_${testname}.plc
+./plcompile -pkginfo -DPLC_DEBUG=1 ../PLC_Extras/Tests/Test_${testname}.plc
 
+result=$?
+if ! [ $result == 0 ] ; then
+	exit 1 
+fi
+ 
 if [ -f ${infoplist} ] ; then
 	cp ${infoplist} ../PLC_Extras/Tests/Output/Info_${testname}.plist
 fi
